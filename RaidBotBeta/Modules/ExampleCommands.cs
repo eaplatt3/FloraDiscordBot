5d1be04ae85e2d45b1f49c9d399fe4729c74af36
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2020-11-24 14:50:19 -0500

    Created "hello" command

--- RaidBotBeta/Modules/ExampleCommands.cs
+++ RaidBotBeta/Modules/ExampleCommands.cs
@@ -1,13 +1,33 @@
 ﻿using Discord;
 using Discord.Net;
-using Discord.WebSocket
+using Discord.WebSocket;
+using Discord.Commands;
 using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 
 namespace RaidBotBeta.Modules
 {
-    class ExampleCommands
+    class ExampleCommands : ModuleBase
     {
+        [Command("hello")]
+        public async Task HelloCommand()
+        {
+            //Initialize empty string builder for reply
+            var sb = new StringBuilder();
+
+            //Get User info from the Context
+            var user = Context.User;
+
+            //Build out the reply
+            sb.AppendLine($"You are -> []");
+            sb.AppendLine("I must now say, World!");
+
+            //Send simple string replay
+            await ReplyAsync(sb.ToString());
+        }
     }
 }
