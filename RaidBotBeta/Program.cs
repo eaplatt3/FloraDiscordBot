75e0e5e7b1a7c802e75a153a79fe5145a67b2bae
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2020-11-24 10:37:09 -0500

    Initial commit

---
+++ RaidBotBeta/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RaidBotBeta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
