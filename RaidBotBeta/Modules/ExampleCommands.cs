92c468da080f3d8e1da7509af3bc62e4801abf0f
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2020-11-24 14:29:40 -0500

    Finished linking CommandHander to Main started Modules

---
+++ RaidBotBeta/Modules/ExampleCommands.cs
@@ -0,0 +1,13 @@
+﻿using Discord;
+using Discord.Net;
+using Discord.WebSocket
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RaidBotBeta.Modules
+{
+    class ExampleCommands
+    {
+    }
+}
